706a6046ab627f82d75acab1bf534b1c73f01806
Eric Baur <baurfamily@gmail.com>
2024-09-21 00:05:34 -0700

    Initial Commit

---
+++ NR Status/NR_StatusApp.swift
@@ -0,0 +1,17 @@
+//
+//  NR_StatusApp.swift
+//  NR Status
+//
+//  Created by Eric Baur on 9/21/24.
+//
+
+import SwiftUI
+
+@main
+struct NR_StatusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NR StatusTests/NR_StatusTests.swift
@@ -0,0 +1,17 @@
+//
+//  NR_StatusTests.swift
+//  NR StatusTests
+//
+//  Created by Eric Baur on 9/21/24.
+//
+
+import Testing
+@testable import NR_Status
+
+struct NR_StatusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

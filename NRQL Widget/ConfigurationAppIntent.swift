24e1509c3284970dcc48511ecc5bacc2e5e4ddf8
Eric Baur <baurfamily@gmail.com>
2024-11-03 18:13:39 -0800

    Making a new widget

---
+++ NRQL Widget/ConfigurationAppIntent.swift
@@ -0,0 +1,38 @@
+//
+//  ConfigurationAppIntent.swift
+//  NRQL Viewer
+//
+//  Created by Eric Baur on 10/12/24.
+//
+
+import WidgetKit
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource { "NRQL Viewer" }
+    static var description: IntentDescription { "This widget allows you to view a graph built from a NRQL query." }
+    
+    @Parameter(title: "API Host", default: "api.newrelic.com")
+    var apiHost: String
+    
+    @Parameter(title: "API Key", default: "NRAK-")
+    var apiKey: String
+    
+    @Parameter(title: "Account IDs", default: "")
+    var accountIds: String
+    
+    @Parameter(title: "Title", default: "NRQL Viewer")
+    var title: String
+    
+    @Parameter(title: "NRQL Query", default: "SELECT count(*) FROM Transaction FACET http.statusCode SINCE 6 hours ago TIMESERIES 15 minutes")
+    var nrqlQuery: String
+    
+    @Parameter(title: "Stacked (does not work with all data sets", default: false)
+    var isStacked: Bool
+    
+    @Parameter(title: "Smoothed", default: true)
+    var isSmoothed: Bool
+    
+    @Parameter(title: "Show Data Points", default: false)
+    var showDataPoints: Bool
+}

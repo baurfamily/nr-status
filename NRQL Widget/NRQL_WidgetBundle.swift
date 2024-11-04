c2304b20ac6f2a8b895798fee0e34366659a0316
Eric Baur <baurfamily@gmail.com>
2024-11-03 18:13:39 -0800

    Making a new widget

---
+++ NRQL Widget/NRQL_WidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  NRQL_WidgetBundle.swift
+//  NRQL Widget
+//
+//  Created by Eric Baur on 11/3/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct NRQL_WidgetBundle: WidgetBundle {
+    var body: some Widget {
+        NRQL_Widget()
+    }
+}

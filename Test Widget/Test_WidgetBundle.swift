286a1da4450d3978aa84d41c4ea8b51f77c7f7c1
Eric Baur <baurfamily@gmail.com>
2025-01-27 21:45:14 -0800

    checkpoing

---
+++ Test Widget/Test_WidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  Test_WidgetBundle.swift
+//  Test Widget
+//
+//  Created by Eric Baur on 11/9/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct Test_WidgetBundle: WidgetBundle {
+    var body: some Widget {
+        Test_Widget()
+    }
+}
